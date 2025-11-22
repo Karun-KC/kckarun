7b7ff7d4791d74c77524f3910b232b2502804393
Karun K C <karun92591@gmail.com>
2025-11-21 23:24:31 -0600

    fix: remove basePath for custom domain, add .nojekyll

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/kckarun',
-  assetPrefix: '/kckarun/',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   trailingSlash: true,
 }
 
